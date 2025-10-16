621edd8fd98df12d89353f89be26aa7160f0e025
nourhan-salah <noursalah565@gmail.com>
2025-10-17 00:24:43 +0300

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,24 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $to = "noursalah565@gmail.com";
+    $headers = "From: " . $email . "\r\n";
+    $headers .= "Reply-To: " . $email . "\r\n";
+    $headers .= "Content-type: text/plain; charset=UTF-8\r\n";
+
+    $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Error sending message.";
+    }
+}
+?>
+
+
+
